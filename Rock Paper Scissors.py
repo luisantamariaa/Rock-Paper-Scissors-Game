2936cbc68763e113cc30f4db6c397e1fd9740557
Luis Santamaria <luis.santamaria@outlook.com>
2022-10-20 09:00:13 -0500

    Add files via upload

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -2,13 +2,15 @@
 
 import random
 
-while True:
-    user_action = input("Enter a choice (rock, paper, scissors): ")
-    possible_actions = ["rock", "paper", "scissors"]
+while True: #we use while to create & execute a loop
+    user_action = input("Enter a choice (rock, paper, scissors): ") #Here we took user's choice on in the game
+    possible_actions = ["rock", "paper", "scissors"] #This are the potential computer elections for chosing
     computer_action = random.choice(possible_actions)
     print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
-    if user_action == computer_action:
+#The below are the potential resolutions to find a who win or lose
+
+    if user_action == computer_action: 
         print(f"Both players selected {user_action}. It's a tie!")
     elif user_action == "rock":
         if computer_action == "scissors":
@@ -26,6 +28,6 @@ while True:
         else:
             print("Rock smashes scissors! You lose.")
 
-    play_again = input("Play again? (y/n): ")
+    play_again = input("Play again? (y/n): ") #we ask the user to play again 
     if play_again.lower() != "y":
         break
